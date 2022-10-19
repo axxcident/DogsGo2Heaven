2f159939ca49d0fc99ab1f8befb0792855ceb852
axxcident <112471629+axxcident@users.noreply.github.com>
2022-10-19 12:29:05 +0200

    lagt till async och await. får forbidden

--- app.js
+++ app.js
@@ -20,18 +20,32 @@ let hund = "Hey now, you're a rockstar";
 
 showMessage(hund);
 
+let dog_api = "http://dog-api.kinduff.com/api/facts";
 
+(async () => {
+  const response = await fetch(dog_api, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    mode: "no-cors"
+  });
+  const data = await response.json();
+  console.log(data);
+})()
 
-
-// Funkar ej pga CORS....
-
-
-
-/* fetch('http://dog-api.kinduff.com/api/facts', {
+/* fetch(dog_api, {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json'
-  }
-})
-  .then(responsen => { return responsen.json() })
-  .then(data => { console.log(data) }) */
+  },
+  mode: "no-cors"
+}).then(responsen => { return responsen.json })
+.then(data => { console.log(data) }) */
+
+
+/* (async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const data = await response.json()
+  console.log(data)
+})() */
