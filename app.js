fb363d39e58df42b9ef1776c3de9b9a7d218a3f2
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2022-10-21 10:27:21 +0200

    Hello_darknesss_myOldFriend

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 
-
+// Vår funktion för text. (och/eller url text)
 function showMessage(meddelande) {
   document.getElementById('randomDog').textContent = meddelande;
 }
 
-let hund = "Hey now, you're a rockstar";
-// showMessage(hund);
-
+// let hund = "Hey now, you're a rockstar";
 // let hund;
-
 // fetch("http://dog-api.kinduff.com/api/facts", {
 //   method: "GET",
 //   headers: {
@@ -18,33 +15,45 @@ let hund = "Hey now, you're a rockstar";
 //   .then(responsen => { return responsen.json() })
 //   .then(data => { console.log(data) })
 
-showMessage(hund);
+// showMessage(hund);
+// let dog_api = "https://dog-api.kinduff.com/api/facts";
 
-let dog_api = "https://dog-api.kinduff.com/api/facts";
-/*
-(async () => {
-  const response = await fetch(dog_api, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    mode: "no-cors"
-  });
-  const data = await response.json();
-  console.log(data);
-})()
-*/
 
-let response = fetch(dog_api);
-console.log("denna får vi som svar....... " + response)
+let quackapi = "https://random-d.uk/api/v2/random";
 
-if (response.ok) { // if HTTP-status is 200-299
+/* let response = fetch(quackapi);
+console.log("denna får vi som svar....... " + response) */
+
+/* if (response.ok) { // if HTTP-status is 200-299
   // get the response body (the method explained below)
-  let json = response.json();
+  let jsoncontainer = response.json();
 } else {
   alert("HTTP-Error: " + response.status);
-}
+} */
 
+
+let nybild;
+
+fetch(quackapi, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  mode: "no-cors"
+}).then(responsen => { return responsen.json })
+  .then(data => { nybild = data.url });
+
+
+getComputedStyle(document.documentElement)
+  .getPropertyValue('--quack-url'); // #999999
+
+document.documentElement.style
+  .setProperty('--quack-url', nybild);
+
+
+
+
+// MODE:NO-CORS försök
 /* fetch(dog_api, {
   method: 'GET',
   headers: {
@@ -55,8 +64,16 @@ if (response.ok) { // if HTTP-status is 200-299
 .then(data => { console.log(data) }) */
 
 
+
+// ASYNC kod försök
 /* (async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-  const data = await response.json()
-  console.log(data)
+  const response = await fetch(dog_api, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    mode: "no-cors"
+  });
+  const data = await response.json();
+  console.log(data);
 })() */
