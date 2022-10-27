d9d6f80918da1ae233fab681ffba14b19c59d519
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2022-10-27 21:37:26 +0200

    Fixat lite sidan. Lagt till img och styling

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 
-// Vår funktion för text. (och/eller url text)
-function showMessage(meddelande) {
+// Vår funktion för text. (och/eller url text) KNSK TA BORT DENNA FUNKTION
+/* function showMessage(meddelande) {
   document.getElementById('randomDog').textContent = meddelande;
 }
-
 let inslag = "woof woof";
+showMessage(inslag); */
 
-showMessage(inslag);
+
+let hundelement = document.querySelector('#dogHome')
+
+console.log(hundelement.getAttribute('src'))
+
+// Fetching image or gif.
+fetch('https://random.dog/woof.json')
+  .then(response => response.json())
+  .then(result => {
+    hundelement.setAttribute('src', result.url)
+  });
+
+console.log(hundelement.getAttribute('src'))
