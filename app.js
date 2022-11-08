70c021f4526f4da33ac220db4c275276a9300c20
axxcident <112471629+axxcident@users.noreply.github.com>
2022-11-08 15:59:52 +0100

    Gnsk nära lösning profil

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ const pubKnappen = document.getElementById("post-publishing");
 // const textfalt = document.getElementById("post-typing");
 const flowet = document.querySelector("body > main > section");
 
+if (JSON.parse(sessionStorage.getItem('mypost')) !== undefined) {
+  var textfalt = JSON.parse(sessionStorage.getItem('mypost'));
+  let nyArtikel = document.createElement('article');
+  nyArtikel.setAttribute("class", "inlagg");
+  nyArtikel.innerHTML += `
+  <i class="fa fa-user fa-2x"></i>
+  <h5 class="post-name">John Doe</h5>
+  <p class="post-text">
+    ${textfalt}
+  </p>
+  `;
+  // markerar översta noden m QS.
+  let toppen = document.querySelector("body > main > section > article:nth-child(2)");
+  // Slänger in nytt inlägg.
+  flowet.insertBefore(nyArtikel, toppen);
+}
+
 // när man klickar på knapp ska formulär-noden köra detta.
 formular.addEventListener("submit", event => {
   // Hindrar sidan från att ladda om. KNSK ta bort och göra AEL knapp så att inläggstext försvinner.
@@ -27,7 +44,11 @@ formular.addEventListener("submit", event => {
   let toppen = document.querySelector("body > main > section > article:nth-child(2)");
   // Slänger in nytt inlägg.
   flowet.insertBefore(nyArtikel, toppen);
+
+  //lägg upp på Session för att spara inlägg.
+  sessionStorage.setItem('mypost', JSON.stringify(textfalt));
 });
+
 
 // Selecta <p> för funktionen.
 let dummyTexts = document.querySelectorAll("#jokeText");
@@ -94,14 +115,22 @@ if (datan !== null) {
   profileLink.setAttribute("src", datan.profileDogPic)
   ctaKnappen.style.display = "none";
 
-
-  mottagaren.innerText = `Hej du måste vara ${datan.firstName} ${datan.lastName} och bo i ${datan.hometown} samt ha ${datan.nrofdogs} hundar`;
-  profilbild.setAttribute("src", datan.profileDogPic)
-  // profilbild.style.backgroundImage = `url(${datan.profileDogPic})`;
-
+  mottagaren.innerText = `
+  <dl>
+    <dt>Namn:</dt>
+    <dd>${datan.firstName} ${datan.lastName}</dd>
+    <dt>Hemstad:</dt>
+    <dd>${datan.hometown}</dd>
+    <dt>Owner of x dogs:</dt>
+    <dd>${datan.nrofdogs}</dd>
+  </dl>
+  `
 } else {
   mottagaren.innerText = ''
 }
+// Hej du måste vara ${datan.firstName} ${datan.lastName} och bo i ${datan.hometown} samt ha ${datan.nrofdogs} hundar`;
+// profilbild.setAttribute("src", datan.profileDogPic)
+// // profilbild.style.backgroundImage = `url(${datan.profileDogPic});
 
 // REFRESH knappen.
 refreshKnappen.addEventListener('click', () => {
