2792665c900ca45e3b92cd79de2c54c47328c564
axxcident <112471629+axxcident@users.noreply.github.com>
2022-11-14 14:33:43 +0100

    Sparat reserv CSV ifall jag får 429 igen

--- start.js
+++ start.js
@@ -1,6 +1,14 @@
 
 const texterna = document.querySelectorAll("#sloganet");
 
+var ageLabels = ["20Y olds", "30Y olds", "40Y olds", "50Y olds", "60Y olds+"];
+var age20 = 0;
+var age30 = 0;
+var age40 = 0;
+var age50 = 0;
+var age60 = 0;
+var agesNum = [];
+
 fetch("https://my.api.mockaroo.com/data.json", {
   headers: {
     "Accept": "application/json",
@@ -9,12 +17,37 @@ fetch("https://my.api.mockaroo.com/data.json", {
 }).then(response => response.text()).then(data => {
   let grejer = [];
   let inlägg = [];
+  let ages = [];
   grejer = data.split("\n").slice(1)
   grejer.forEach(grej => {
     kolumn = grej.split(",");
-    // console.log(kolumn[4])
+    // Array med alla slogans
     inlägg.push(kolumn[4])
+    //Array med alla åldrar - KOLLA OM DENNA FUNKAR SENARE
+    ages.push(Number(kolumn[7]))
   })
+  ages.forEach(age => {
+    if (age < 30) {
+      age20++
+    }
+    else if (age >= 30 && age < 40) {
+      age30++
+    }
+    else if (age >= 40 && age < 50) {
+      age40++
+    }
+    else if (age >= 50 && age < 60) {
+      age50++
+    }
+    else if (age > 60) {
+      age60++
+    }
+  })
+  agesNum.push(age20);
+  agesNum.push(age30);
+  agesNum.push(age40);
+  agesNum.push(age50);
+  agesNum.push(age60);
   setInterval(() => {
     // let index = 0;
     let slump = parseInt((Math.random() * 90))
@@ -24,7 +57,8 @@ fetch("https://my.api.mockaroo.com/data.json", {
       sake.innerHTML = inlägg[slump];
       slump++
     })
-  }, 8000);
+  }, 7900);
+  console.log(agesNum)
 })
 
 
